e368e4e22356c8af365068fbc7b14c6a6fea3c74
Mildly_Interested <mueller.manuel@mailbox.org>
2022-04-07 20:55:45 +0200

    go away regex

--- Workshop_Notifier/preset_2_modIDs.py
+++ Workshop_Notifier/preset_2_modIDs.py
@@ -1,16 +1,22 @@
-import re
+def whiteSpaceData(leading_up, line, follow=" "): #find number thats following the passed string in the passed line, follow is following characters
+    data = line[line.find(leading_up)+len(leading_up):line.find(follow,line.find(leading_up)+len(leading_up))]
+    return data
 
-pattern = re.compile("\?id=[0-9]+")
-dirty_list = []
-clean_numbers = []
+mod_id_list = []
 
-with open ('preset.html', mode='rt', errors='replace') as myfile:    
-    for line in myfile:
-        if pattern.search(line) != None:
-            dirty_numbers = re.findall("[0-9]+", line)
-            dirty_numbers_unique = set(dirty_numbers)
-            dirty_list.append(dirty_numbers_unique)
+print('Please enter path to/drag preset file into here:')
+path_2_file = input()
+if path_2_file[0] == '&':
+    path_2_file = path_2_file[3:]
+    path_2_file = path_2_file[:-1]
+print(path_2_file)
 
-dirty_string = str(dirty_list)
-clean_numbers = re.sub(r"[\[\]\'\s\{\}]", '',dirty_string)
-print(clean_numbers)
+with open(path_2_file, 'r') as file:
+    for line in file:
+        if (line.find("http://steamcommunity.com/sharedfiles")>0):
+            mod_id = whiteSpaceData("?id=", line,'\"')
+            #print(mod_id)
+            mod_id_list.append(mod_id)
+
+mod_str = ','.join(mod_id_list)
+print(mod_str)
--- map_issues.md
+++
@@ -1,6 +0,0 @@
-**Maps**
-- Isla Duala 
-    crap FPS looking east 
-    https://discord.com/channels/154937272368758784/291690109760176128/824297086991204352
-- VIDDA
-    lots of rocks lowering FPS a lot
